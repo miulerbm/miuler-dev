91e02b36e6af676756b3bfdc3f651f00340a843e
miulerbm <miulerbm00@gmail.com>
2024-05-05 15:13:29 -0500

    SiteHeader

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "Miuler Blog",
+  url: "https://example.com",
+  description: "Nextjs 14 blog using velite, tailwind and shadcn",
+  author: "miulerbm",
+  links: {
+    twitter: "https://twitter.com/blas_miuler",
+    github: "https://github.com/miulerbm",
+    personalSite: "https://miulerbm.vercel.app/",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
