37205e28810eb7367b50554816794a6d19f7e26d
miulerbm <miulerbm00@gmail.com>
2024-05-06 20:00:54 -0500

    Add tags to post items

---
+++ components/tag.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { slug } from "github-slugger";
+import { badgeVariants } from "./ui/badge";
+
+// What slugger does: rehype pretty => (rehype-pretty)
+
+interface TagProps {
+  tag: string;
+  current?: boolean;
+  count?: number;
+}
+
+export function Tag({ tag, current, count }: TagProps) {
+  return (
+    <Link
+      className={badgeVariants({
+        variant: current ? "default" : "secondary",
+        className: "no-underline rounded-md",
+      })}
+      href={`/tags/${slug(tag)}`}
+    >
+      {tag}
+    </Link>
+  );
+}
