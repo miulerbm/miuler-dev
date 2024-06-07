bd16dec9107c6760cdce5c4425584465914f47f8
miulerbm <miulerbm00@gmail.com>
2024-06-07 18:44:28 -0500

    new openg image

--- app/[locale]/blog/[...slug]/page.tsx
+++ app/[locale]/blog/[...slug]/page.tsx
@@ -48,19 +48,19 @@ export async function generateMetadata({
       url: post.slug,
       images: [
         {
-          url: `/api/og?${ogSearchParams.toString()}`,
+          url: `${process.env.NEXT_PUBLIC_APP_URL}images/og-image.jpg`,
           width: 1200,
           height: 630,
           alt: post.title,
         },
       ],
     },
-    twitter: {
-      card: "summary_large_image",
-      title: post.title,
-      description: post.description,
-      images: [`/api/og?${ogSearchParams.toString()}`],
-    },
+    // twitter: {
+    //   card: "summary_large_image",
+    //   title: post.title,
+    //   description: post.description,
+    //   images: [`/api/og?${ogSearchParams.toString()}`],
+    // },
   };
 }
 
