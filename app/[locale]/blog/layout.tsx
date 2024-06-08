5a186bf8d6cc7bd8c7e8a4c24df000562f69cc74
miulerbm <miulerbm00@gmail.com>
2024-06-07 19:49:42 -0500

    Adding openG for blog page

--- app/[locale]/blog/layout.tsx
+++ app/[locale]/blog/layout.tsx
@@ -7,6 +7,19 @@ import React from "react";
 export const metadata: Metadata = {
   title: "Miuler's blog",
   description: "Bits of my developer path.",
+  openGraph: {
+    title: "Blog",
+    description: "My blog",
+    type: "article",
+    images: [
+      {
+        url: `${process.env.NEXT_PUBLIC_APP_URL}images/og-blog.jpg`,
+        width: 1200,
+        height: 630,
+        alt: "Blogs image",
+      },
+    ],
+  },
 };
 
 export default function BlogLayout({
