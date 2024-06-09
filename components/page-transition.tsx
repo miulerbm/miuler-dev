45a4f14d4594ae91085afd0313e002a6d3ffcdec
miulerbm <miulerbm00@gmail.com>
2024-06-09 09:31:21 -0500

    Reveal transiton

--- components/page-transition.tsx
+++ components/page-transition.tsx
@@ -1,25 +1,23 @@
 "use client";
 import { motion, AnimatePresence } from "framer-motion";
 import { usePathname } from "next/navigation";
-import { ReactNode, useEffect } from "react";
+import { ReactNode } from "react";
 
 export default function PageTransition({ children }: { children: ReactNode }) {
   const pathname = usePathname();
-
-  useEffect(() => {
-    window.scrollTo(0, 0); // Desplaza la página hacia arriba
-  }, [pathname]);
 
   return (
     <AnimatePresence mode="wait">
       <motion.div
         key={pathname}
         variants={{
-          initial: { opacity: 0, y: 100 },
-          animate: { opacity: 1, y: 0 },
+          initial: { opacity: 1, clipPath: "inset(0% 0% 100% 0%)" },
+          animate: { opacity: 1, clipPath: "inset(0% 0% 0% 0%)" },
         }}
         initial="initial"
         animate="animate"
+        exit="exit"
+        transition={{ ease: "easeOut", duration: 2 }}
       >
         {children}
       </motion.div>
